d92ebe1d8e78d378578fd18ec4561dc39e88a472
Sakib Mustafa <147903390+sakib-mustafa-ak@users.noreply.github.com>
2026-06-09 10:32:01 +0600

    Add NumberChecker class to check even numbers

--- Task 2/YouDo2.java
+++ Task 2/YouDo2.java
@@ -1,1 +1,14 @@
 
+class NumberChecker{
+    void check (int num){
+        boolean isEvenAndGreaterThanTen = (num %2==0) &&(num>10);
+        System.out.println(isEvenAndGreaterThanTen);
+    }
+}
+
+public class main2 {
+    public static void main(String[] args) {
+        NumberChecker checker = new NumberChecker();
+        checker.check(14);
+    }
+}
