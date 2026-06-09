0aa8466d21b3338ef03cc8b7afff3b01c81c3017
Sakib Mustafa <147903390+sakib-mustafa-ak@users.noreply.github.com>
2026-06-09 10:35:01 +0600

    Implement Item and Order classes with billing

--- Task 2/YouDo8.java
+++ Task 2/YouDo8.java
@@ -1,1 +1,40 @@
 
+
+class Item{
+    String name;
+    double price;
+
+    Item (String name, double price){
+        this.name = name;
+        this.price = price;
+    }
+}
+
+class Order{
+    Item item1;
+    Item item2;
+
+    Order(Item item1, Item item2){
+        this.item1 = item1;
+        this.item2 = item2;
+    }
+
+    void printBill(){
+        double total = item1.price + item2.price;
+        System.out.println("--- Order List ---");
+        System.out.println(item1.name + ": $" + item1.price);
+        System.out.println(item2.name + ": $" + item2.price);
+        System.out.println("Total cost: $" + total);
+    }
+}
+
+public class main8 {
+    public static void main(String[] args) {
+        Item mouse = new Item("Mouse", 20.0);
+        Item keyboard = new Item("Keyboard", 45.0);
+
+        Order myOrder = new Order(mouse,keyboard);
+        myOrder.printBill();
+    }
+    
+}
