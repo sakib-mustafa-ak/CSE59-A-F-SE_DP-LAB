1d610ec748866737bdf668886332d59c04fd1036
Sakib Mustafa <147903390+sakib-mustafa-ak@users.noreply.github.com>
2026-06-09 10:32:41 +0600

    Implement Sensor class with charge method
    Added Sensor class with charging functionality and battery level display.

--- Task 2/YouDo4.java
+++ Task 2/YouDo4.java
@@ -1,1 +1,18 @@
 
+class Sensor{
+    String name = "TS";
+    int batteryLevel;
+
+    void charge(){
+        batteryLevel = 100;
+        System.out.println("Sensor fully charged");
+    }
+}
+
+public class main4 {
+    public static void main(String[] args) {
+        Sensor s = new Sensor();
+        s.charge();
+        System.out.println("Current Battery Level: " + s.batteryLevel);
+    }
+}
