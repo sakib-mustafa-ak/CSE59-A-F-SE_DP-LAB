9965ac3b5083f64d94a613e815bac32a5eb1d82d
Sakib Mustafa <147903390+sakib-mustafa-ak@users.noreply.github.com>
2026-06-23 10:04:14 +0600

    Update Main2.java

--- Task 4/Main2.java
+++ Task 4/Main2.java
@@ -1,1 +1,40 @@
 
+public class Employee {
+    protected String name;
+    
+    public Employee(String name) { this.name = name; }
+    
+    public String getName(){
+        return name;
+    }
+
+    public void calculateBonus() {
+        System.out.println("Calculating standard employee bonus...");
+    }
+}
+
+public interface BonusEligible {
+    void calculateBonus();
+}
+
+public class PermanentEmployee extends Employee implements BonusEligible{
+    public PermanentEmployee(String name){
+        super(name);
+    }
+
+    @Override
+    public void calculateBonus(){
+        System.out.println("Calculating standard employee bonus......");
+    }
+}
+
+public class Contractor extends Employee{
+    public Contractor(String name){
+        super(name);
+    }
+}
+
+
+public class Main2 {
+    
+}
